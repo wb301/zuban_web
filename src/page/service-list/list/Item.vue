255d2bd31f840d5cc1fea3ed741cd84bd4f087ef
沈冬旭 <18001637008@163.com>
2017-02-23 02:06:18 +0800

    fix

--- src/page/service-list/list/Item.vue
+++ src/page/service-list/list/Item.vue
@@ -4,8 +4,25 @@
             <div class="wapper">
                 <div class="img-wapper"><img :src="item.product_image"></div>
                 <div class="info-wapper">
-                    <div>1</div>
-                    <div>2</div>
+                    <div>
+                        <div class="info-top">
+                            <div>
+                                <span class="title">{{item.product_info}}</span>
+                                <span class="juli">{{item.juli}}公里</span>
+                            </div>
+                            <div>
+                                <span class="name">{{item.userInfo.nick_name}}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="info-bottom">
+                        <div class="price">
+                            <span>￥</span>
+                            <span>{{item.price}}</span>
+                            <span>/小时</span>
+                        </div>
+                        <div class="content">白天有需要逛街的可以联系我哈~帮选购，帮砍价，还有看电影</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -22,7 +39,10 @@ export default {
     created: function() {
 
     },
-    mounted() {},
+    mounted() {
+        this.item.juli = parseFloat(this.item.juli / 1000).toFixed(1);
+        this.item.price = parseInt(this.item.price);
+    },
     methods: {},
     destroyed() {}
 }
@@ -53,6 +73,49 @@ export default {
             right: 0;
             top: 12.5px;
             bottom: 15px;
+            >div {
+                height: 45px;
+            }
+            .info-top {
+                >div {
+                    line-height: 15px;
+                }
+                .title {
+                    font-size: 13px;
+                    color: #333;
+                }
+                .name {
+                    font-size: 10px;
+                    color: #999;
+                }
+                .juli {
+                    font-size: 10px;
+                    color: #999;
+                    position: absolute;
+                    right: 10px;
+                }
+            }
+            .info-bottom {
+                >div {
+                    line-height: 15px;
+                }
+                .price {
+                    color: #A878E5;
+                    span:nth-child(2)(1) {
+                        font-size: 12px;
+                    }
+                    span:nth-child(2) {
+                        font-size: 17px;
+                    }
+                    span:nth-child(3) {
+                        font-size: 10px;
+                    }
+                }
+                .content {
+                    font-size: 10px;
+                    color: #999;
+                }
+            }
         }
     }
 }
