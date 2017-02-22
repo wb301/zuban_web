5d8e076b5e29897296b3804f85e5e0c84ea09645
何娇凤 <January.he@gmail.com>
2017-02-22 21:58:25 +0800

    navbar

--- src/page/service-list/list/Index.vue
+++ src/page/service-list/list/Index.vue
@@ -1,16 +1,30 @@
 <template>
     <div>
         <div class="container-body">
-        11
+            <div class="weui-tab">
+                <div class="weui-navbar">
+                    <div class="weui-navbar__item">
+                        静安寺
+                        <img :src="image" />
+                    </div>
+                    <div class="weui-navbar__item">
+                        分类
+<img :src="image" />                    </div>
+                    <div class="weui-navbar__item">
+                        排序
+<img :src="image" />                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
+import xiala from './image/xiala.png'
 export default {
     components: {},
     data() {
         return {
-
+            image: xiala
         }
     },
     mounted() {
@@ -23,4 +37,11 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+    .weui-tab{
+        background: #FFFFFF;
+    }
+    img{
+        width: 12px;
+        height: 12px;
+    }
 </style>
