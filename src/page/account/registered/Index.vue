fe54a13f5b2a9b79ad498cfff2aa2b4f40690b11
何娇凤 <January.he@gmail.com>
2017-02-22 14:08:46 +0800

    手机号验证

--- src/page/account/registered/Index.vue
+++ src/page/account/registered/Index.vue
@@ -92,6 +92,10 @@ export default {
             var param = {
                 mobile: this.mobile
             };
+            if(!(/^1(3|4|5|7|8)\d{9}$/.test(param.mobile))){ 
+                weui.alert("手机号码有误，请重填");  
+                return false; 
+            };
             var p_obj = {
                 action: 'c=Zb&m=User&a=sendMobileCode',
                 param: param,
