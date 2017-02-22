11c4f250de55a9c7b259cffdbdd7e3c9d99058fe
何娇凤 <January.he@gmail.com>
2017-02-22 22:25:52 +0800

    排序picker

--- src/page/service-list/list/Index.vue
+++ src/page/service-list/list/Index.vue
@@ -9,10 +9,12 @@
                     </div>
                     <div class="weui-navbar__item">
                         分类
-<img :src="image" />                    </div>
-                    <div class="weui-navbar__item">
+                        <img :src="image" /> 
+                    </div>
+                    <div class="weui-navbar__item" @click="sort">
                         排序
-<img :src="image" />                    </div>
+                        <img :src="image" /> 
+                    </div>
                 </div>
             </div>
         </div>
@@ -24,13 +26,25 @@ export default {
     components: {},
     data() {
         return {
-            image: xiala
+            image: xiala,
+            area: '',
+            areaList: []
         }
     },
     mounted() {
 
     },
     methods: {
+        sort(){
+            var arr = [{label:"最近时间",value:'mr'},{label:"价格低到高",value:'jg_0'},{label:"价格从低到高",value:'jg_0'},{label:"价格从高到低",value:'jg_1'},{label:"距离从近到远",value:'jl_0'},{label:"距离从远到近",value:'jl_1'},{label:"智能综合",value:'zh'}];
+            weui.picker(arr, {
+                className: 'custom-classname',
+                onConfirm: function(result) {
+                   console.log(result);
+                },
+                id: 'sortPicker'
+            });
+        }
 
     },
     destroyed() {}
