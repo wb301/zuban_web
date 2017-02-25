25bcc8d522e98f840c0dab4c8f927404cdea583b
何俊宏 <704492613@qq.com>
2017-02-25 20:57:45 +0800

    我的详情页

--- src/page/my-info/Index.vue
+++ src/page/my-info/Index.vue
@@ -4,31 +4,34 @@
         <div class="weui-cells" style = "margin-top:0px;">
             <a class="weui-cell weui-cell_access" href="javascript:;">
                 <div class="weui-cell__bd">
-                    <p>头像</p>
+                    <img :src="userInfo.head_img" style="width: 75px;height:75px;"/>
                 </div>
                 <div class="weui-cell__ft">
-                    <img :src="userInfo.head_img" style="width: 75px;height:75px;"/>
                 </div>
             </a>
         </div>
 
-        <div class="weui-cells" style = "margin-top:7.5px;">
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__bd">
-                    <p>昵称</p>
+        <!-- 昵称 性别 -->
+        <div class="content-manipulation" style = "margin-top:2.5px;">
+            <div class="weui-cells">
+                <div class="weui-cell">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">昵称</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        <input class="weui-input" type="test" placeholder="未设置" v-model="nick_name" />
+                    </div>
                 </div>
-                <div class="weui-cell__ft">
-                    {{userInfo.nick_name}}
+                <div class="weui-cell">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">性别</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        <input class="weui-input disabled-input" type="text" v-model="sex" @click="selectSex" />
+                    </div>
+                    <div><img :src="image"></div>
                 </div>
-            </a>
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__bd">
-                    <p>性别</p>
-                </div>
-                <div class="weui-cell__ft">
-                    {{userInfo.sex == 'W' ? '女' : '男'}}
-                </div>
-            </a>
+            </div>
         </div>
 
         <div class="weui-cells" style = "margin-top:7.5px;">
@@ -104,19 +107,42 @@
 <script>
 import xiala from '../service-list/list/image/xiala.png'
 import shouji from '../service-list/list/image/shouji.png'
+
+var sexArr = [{
+    label: "男",
+    value: 'M'
+}, {
+    label: "女",
+    value: 'W'
+}];
 export default {
     components: {
         
     },
     data() {
         return {
+            image : xiala,
             userInfo: NormalHelper.userInfo()
         }
     },
     mounted() {
         this.getUserInfo()
+        this.nick_name = this.userInfo.nick_name
+        this.sex = this.userInfo.sex == 'W' ? '女' : '男'
     },
     methods: {
+        selectSex() {
+            var _self = this;
+            weui.picker(sexArr, {
+                defaultValue: ['W'],
+                className: 'custom-classname',
+                onConfirm: function(result) {
+                    _self.danweiValue = result[0].value;
+                    _self.danweiName = result[0].label;
+                },
+                id: 'unitPricePicker'
+            });
+        },
         getUserInfo() {
 
             var param = {
