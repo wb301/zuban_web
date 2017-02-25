2d4651d3672256192d4ee2a90eb9895a1d5ce503
何俊宏 <704492613@qq.com>
2017-02-25 22:35:08 +0800

    我的详情

--- src/page/my-info/Index.vue
+++ src/page/my-info/Index.vue
@@ -12,14 +12,14 @@
         </div>
 
         <!-- 昵称 性别 -->
-        <div class="content-manipulation" style = "margin-top:2.5px;">
+        <div class="content-manipulation">
             <div class="weui-cells">
                 <div class="weui-cell">
                     <div class="weui-cell__hd">
                         <label class="weui-label">昵称</label>
                     </div>
                     <div class="weui-cell__bd">
-                        <input class="weui-input" type="test" placeholder="未设置" v-model="nick_name" />
+                        <input class="weui-input" type="test" placeholder="未设置" v-model="nick_name_model" />
                     </div>
                 </div>
                 <div class="weui-cell">
@@ -27,80 +27,92 @@
                         <label class="weui-label">性别</label>
                     </div>
                     <div class="weui-cell__bd">
-                        <input class="weui-input disabled-input" type="text" v-model="sex" @click="selectSex" />
+                        <input class="weui-input disabled-input" type="text" v-model="sex_model" @click="selectSex" />
                     </div>
                     <div><img :src="image"></div>
                 </div>
             </div>
         </div>
 
-        <div class="weui-cells" style = "margin-top:7.5px;">
-            <a class="weui-cell weui-cell_access">
-                <div class="weui-cell__bd">
-                    <p>所在地</p>
+        <div class="content-manipulation">
+            <div class="weui-cells">
+
+                <div class="weui-cell">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">所在地</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        {{userInfo.region_name}}
+                    </div>
                 </div>
-                <div class="weui-cell__ft">
-                    {{userInfo.region_name}}
+
+                <div class="weui-cell">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">绑定手机</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        {{userInfo.account}}
+                    </div>
                 </div>
-            </a>
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__bd">
-                    <p>绑定手机</p>
+
+                <div class="weui-cell">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">微信号</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        <input class="weui-input" type="test" placeholder="未设置" v-model="wx_account_model" />
+                    </div>
                 </div>
-                <div class="weui-cell__ft">
-                    {{userInfo.account}}
+
+                <div class="weui-cell">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">年龄</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        <input class="weui-input disabled-input" placeholder="未设置" type="text" v-model="age_model" />
+                    </div>
                 </div>
-            </a>
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__bd">
-                    <p>微信号</p>
+
+                <div class="weui-cell">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">身高</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        <input class="weui-input" type="test" placeholder="未设置" v-model="height_model" />
+                    </div>
                 </div>
-                <div class="weui-cell__ft">
-                    {{userInfo.wx_account ? userInfo.wx_account : '未设置'}}
+
+                <div class="weui-cell">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">体重</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        <input class="weui-input" type="test" placeholder="未设置" v-model="weight_model" />
+                    </div>
                 </div>
-            </a>
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__bd">
-                    <p>年龄</p>
+
+                <div class="weui-cell">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">学历</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        <input class="weui-input" type="test" placeholder="未设置" v-model="qualifications_model" />
+                    </div>
                 </div>
-                <div class="weui-cell__ft">
-                    {{userInfo.age > 0 ? userInfo.age : '未设置'}}
+
+                <div class="weui-cell">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">职业</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        <input class="weui-input" type="test" placeholder="未设置" v-model="professional_model" />
+                    </div>
                 </div>
-            </a>
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__bd">
-                    <p>身高</p>
-                </div>
-                <div class="weui-cell__ft">
-                    {{userInfo.height > 0 ? userInfo.height : '未设置'}}
-                </div>
-            </a>
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__bd">
-                    <p>体重</p>
-                </div>
-                <div class="weui-cell__ft">
-                    {{userInfo.weight > 0 ? userInfo.weight : '未设置'}}
-                </div>
-            </a>
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__bd">
-                    <p>学历</p>
-                </div>
-                <div class="weui-cell__ft">
-                    {{userInfo.qualifications ? userInfo.qualifications : '未设置'}}
-                </div>
-            </a>
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__bd">
-                    <p>职业</p>
-                </div>
-                <div class="weui-cell__ft">
-                    {{userInfo.professional ? userInfo.professional : '未设置'}}
-                </div>
-            </a>
+
+            </div>
         </div>
 
+        <a @click="updUserInfo" class="weui-btn weui-btn_primary">修改并保存</a>
 
     </div>
 </template>
@@ -122,23 +134,45 @@ export default {
     data() {
         return {
             image : xiala,
+            nick_name_model : '',
+            sex  : '',
+            sex_model : '',
+            wx_account_model : '',
+            qualifications_model : '',
+            professional_model : '',
+            age_model : '',
+            height_model : '',
+            weight_model : '',
             userInfo: NormalHelper.userInfo()
         }
     },
     mounted() {
         this.getUserInfo()
-        this.nick_name = this.userInfo.nick_name
-        this.sex = this.userInfo.sex == 'W' ? '女' : '男'
+        this.nick_name_model = this.userInfo.nick_name
+        this.sex = this.userInfo.sex
+        this.sex_model = this.sex == 'W' ? '女' : '男'
+        this.wx_account_model = this.userInfo.wx_account
+        this.qualifications_model = this.userInfo.qualifications
+        this.professional_model = this.userInfo.professional
+        if(this.userInfo.age > 0){
+            this.age_model = this.userInfo.age
+        }
+        if(this.userInfo.height > 0){
+            this.height_model = this.userInfo.height
+        }
+        if(this.userInfo.weight > 0){
+            this.weight_model = this.userInfo.weight
+        }
     },
     methods: {
         selectSex() {
             var _self = this;
             weui.picker(sexArr, {
-                defaultValue: ['W'],
+                defaultValue: ['1'],
                 className: 'custom-classname',
                 onConfirm: function(result) {
-                    _self.danweiValue = result[0].value;
-                    _self.danweiName = result[0].label;
+                    _self.sex = result[0].value;
+                    _self.sex_model = result[0].label;
                 },
                 id: 'unitPricePicker'
             });
@@ -150,6 +184,57 @@ export default {
             };
             var p_obj = {
                 action: 'c=Zb&m=User&a=getUserInfo',
+                param: param,
+                success: (response) => {
+                    response["token"] = this.userInfo.token;
+                    NormalHelper.setUserInfo(response);
+                    this.userInfo = response;
+                },
+                fail: (response) => {
+                    weui.alert(response.msg)
+                }
+            };
+            AjaxHelper.PostRequest(p_obj);
+        },
+        updUserInfo() {
+
+            var param = {"token" : this.userInfo.token,"sex" : this.sex};
+            if(this.nick_name_model){
+                param["nick_name"] = this.nick_name_model;
+            }
+            if(this.wx_account_model){
+                param["wx_account"] = this.wx_account_model;
+            }
+            if(this.qualifications_model){
+                param["qualifications"] = this.qualifications_model;
+            }
+            if(this.professional_model){
+                param["professional"] = this.professional_model;
+            }
+            if(this.age_model){
+                if (!(/^[1-9]\d$/.test(this.age_model)) || this.age_model > 100) {
+                    weui.alert("年龄在1~100之间");
+                    return false;
+                };
+                param["age"] = this.age_model;
+            }
+            if(this.height_model){
+                if (!(/^[1-9]\d{2}$/.test(this.height_model)) || this.height_model > 500) {
+                    weui.alert("身高在0~260之间");
+                    return false;
+                };
+                param["height"] = this.height_model;
+            }
+            if(this.weight_model){
+                if (!(/^[1-9]\d{2}$/.test(this.weight_model)) || this.weight_model > 500) {
+                    weui.alert("体重在50~500之间");
+                    return false;
+                };
+                param["weight"] = this.weight_model;
+            }
+
+            var p_obj = {
+                action: 'c=Zb&m=User&a=updUserInfo',
                 param: param,
                 success: (response) => {
                     response["token"] = this.userInfo.token;
