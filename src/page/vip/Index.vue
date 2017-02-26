a5e8d1fa94f5f1eceec73bd172a93181bdf67242
何俊宏 <704492613@qq.com>
2017-02-26 15:14:06 +0800

    充值VIP

--- src/page/vip/Index.vue
+++ src/page/vip/Index.vue
@@ -2,8 +2,8 @@
     <div style = "background: #F5F5F5;">
 
         <!-- 当前等级 会员特权 -->
-        <div class="content-manipulation" style = "margin-top:0px;">
-            <div class="weui-cells" style="margin-top: 8px">
+        <div class="content-manipulation">
+            <div class="weui-cells" style="margin-top: 0px">
                 <div class="weui-cell class_height">
                     <div class="weui-cell__hd">
                         <label class="weui-label">当前等级</label>
@@ -36,14 +36,10 @@
 
         </div>
 
-        <!-- <div v-if={{this.is_vip}}>
-            你好
-        </div> -->
-
         <!-- 开通会员 -->
-        <!-- <div style="margin-top: 10px">
+        <div style="margin-top: 10px" v-if="is_vip == false">
             <label class="weui-label">开通会员</label>
-            <div >
+            <div>
                 <div class="dropload-wapper">
                     <div>
                         <list-item v-for="(item,index) in vipList" :item="item"></list-item>
@@ -51,7 +47,7 @@
                 </div>
             </div>
 
-        </div>    -->     
+        </div>        
 
     </div>
 </template>
@@ -61,14 +57,14 @@ import ListItem from './item'
 
 export default {
     components: {
-        
+        ListItem
     },
     data() {
         return {
             image : xiala,
             vip_level : '注册会员',
             vip_info  : '无',
-            is_vip : false,
+            is_vip : NormalHelper.userInfo()["vip"] ? true : false,
             vipList  : [],
             vipInfo: NormalHelper.userInfo()["vip"]
         }
@@ -93,74 +89,15 @@ export default {
                     userInfo["vip"] = this.vipInfo;
                     NormalHelper.setUserInfo(userInfo);
 
-                    if(this.vipInfo){
+                    for (var i = 0; i < this.vipConfig.length; i++) {
+                        if(this.vipInfo && this.vipInfo["vip_type"] == this.vipConfig[i]["level"]){
 
-                        for (var i = 0; i < this.vipConfig.length; i++) {
-                            if(this.vipInfo["vip_type"] == this.vipConfig[i]["level"]){
-                                this.vip_level = this.vipConfig[i]["name"];
-                                this.vip_info = "可在"+this.vipInfo["start_time"]+"至"+this.vipInfo["end_time"]+"内免费查看出租人的联系方式";
-                            }
-                            this.vipList.push(this.vipConfig[i]);
+                            this.is_vip = true;
+                            this.vip_level = this.vipConfig[i]["name"];
+                            this.vip_info = "可在"+this.vipInfo["start_time"]+"至"+this.vipInfo["end_time"]+"内免费查看出租人的联系方式";
                         }
+                        this.vipList.push(this.vipConfig[i]);
                     }
-                    console.log(response);
-
-
-
-                    // response["token"] = this.userInfo.token;
-                    // NormalHelper.setUserInfo(response);
-                    // this.userInfo = response;
-                },
-                fail: (response) => {
-                    weui.alert(response.msg)
-                }
-            };
-            AjaxHelper.PostRequest(p_obj);
-        },
-        updUserInfo() {
-
-            var param = {"token" : this.userInfo.token,"sex" : this.sex};
-            if(this.nick_name_model){
-                param["nick_name"] = this.nick_name_model;
-            }
-            if(this.wx_account_model){
-                param["wx_account"] = this.wx_account_model;
-            }
-            if(this.qualifications_model){
-                param["qualifications"] = this.qualifications_model;
-            }
-            if(this.professional_model){
-                param["professional"] = this.professional_model;
-            }
-            if(this.age_model){
-                if (!(/^[1-9]\d$/.test(this.age_model)) || this.age_model > 100) {
-                    weui.alert("年龄在1~100之间");
-                    return false;
-                };
-                param["age"] = this.age_model;
-            }
-            if(this.height_model){
-                if (!(/^[1-9]\d{2}$/.test(this.height_model)) || this.height_model > 500) {
-                    weui.alert("身高在0~260之间");
-                    return false;
-                };
-                param["height"] = this.height_model;
-            }
-            if(this.weight_model){
-                if (!(/^[1-9]\d{2}$/.test(this.weight_model)) || this.weight_model > 500) {
-                    weui.alert("体重在50~500之间");
-                    return false;
-                };
-                param["weight"] = this.weight_model;
-            }
-
-            var p_obj = {
-                action: 'c=Zb&m=User&a=updUserInfo',
-                param: param,
-                success: (response) => {
-                    response["token"] = this.userInfo.token;
-                    NormalHelper.setUserInfo(response);
-                    this.userInfo = response;
                 },
                 fail: (response) => {
                     weui.alert(response.msg)
--- src/page/vip/Item.vue
+++ src/page/vip/Item.vue
@@ -1,29 +1,9 @@
 <template>
     <div>
-        <div class="item-entry">
-            <div class="wapper">
-                <!-- <div class="img-wapper"><img :src="item.product_image"></div> -->
-                <div class="info-wapper">
-                    <div>
-                        <div class="info-top">
-                            <div>
-                                <span class="title">{{item.name}}</span>
-                            </div>
-                            <div>
-                                <!-- <span class="name">{{item.userInfo.nick_name}}</span> -->
-                            </div>
-                        </div>
-                    </div>
-                    <div class="info-bottom">
-                        <div class="price">
-                            <span>￥</span><!-- 
-                            <span>{{item.price}}</span>
-                            <span>/{{item.danwei}}</span> -->
-                        </div>
-                        <!-- <div class="content">{{item.product_info}}</div> -->
-                    </div>
-                </div>
-            </div>
+        <div class="item-cell">
+            
+            <p style="float: left;margin-left: 10px;margin-top: 2px">{{item.name}}￥{{item.price}}元</p>
+            <a @click="payVip(item)" class="weui-btn weui-btn_mini weui-btn_warn" style="float: right;margin-right: 10px">充值</a>
         </div>
     </div>
 </template>
@@ -35,89 +15,37 @@ export default {
     data() {
         return {}
     },
-    created: function() {
-        // this.item.juli = parseFloat(this.item.juli / 1000).toFixed(1);
-        // this.item.danwei = "小时";
-        // if (this.item.price_type == 2) {
-        //     this.item.danwei = "天";
-        // }else if(this.item.price_type == 3){
-        //     this.item.danwei = "次";
-        // }
+    methods: {
+        payVip(item) {
+
+            var param = {
+                token: NormalHelper.userInfo()["token"],
+                vip: item.level,
+                source: 1,
+                allPrice: item.price,
+                paymentAry: JSON.stringify({"pay_type":"WX"})
+            };
+            console.log(JSON.stringify(param));
+            var p_obj = {
+                action: 'c=Zb&m=Order&a=createMembersOrder',
+                param: param,
+                success: (response) => {
+                    
+                    alert("下单成功!" + JSON.stringify(response));
+                },
+                fail: (response) => {
+                    weui.alert(response.msg)
+                }
+            };
+            AjaxHelper.PostRequest(p_obj);
+        }
     },
-    methods: {},
     destroyed() {}
 }
 </script>
 <style lang="less" scoped>
-.item-entry {
-    height: 117.5px;
-    position: relative;
-    .wapper {
-        border-bottom: 1px solid #e2e2e2;
-        position: absolute;
-        right: 0;
-        left: 15px;
-        top: 0;
-        bottom: 0;
-        padding-top: 12.5px;
-        .img-wapper {
-            width: 90px;
-            height: 90px;
-            img {
-                width: 90px;
-                height: 90px;
-            }
-        }
-        .info-wapper {
-            position: absolute;
-            left: 100px;
-            right: 0;
-            top: 12.5px;
-            bottom: 15px;
-            >div {
-                height: 45px;
-            }
-            .info-top {
-                >div {
-                    line-height: 15px;
-                }
-                .title {
-                    font-size: 13px;
-                    color: #333;
-                }
-                .name {
-                    font-size: 10px;
-                    color: #999;
-                }
-                .juli {
-                    font-size: 10px;
-                    color: #999;
-                    position: absolute;
-                    right: 10px;
-                }
-            }
-            .info-bottom {
-                >div {
-                    line-height: 15px;
-                }
-                .price {
-                    color: #A878E5;
-                    span:nth-child(1) {
-                        font-size: 12px;
-                    }
-                    span:nth-child(2) {
-                        font-size: 17px;
-                    }
-                    span:nth-child(3) {
-                        font-size: 10px;
-                    }
-                }
-                .content {
-                    font-size: 10px;
-                    color: #999;
-                }
-            }
-        }
-    }
+.item-cell {
+    height: 45px;
+    width: 100%;
 }
 </style>
