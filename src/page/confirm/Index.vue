466561dd502ae032d241e55228624df478074836
沈冬旭 <18001637008@163.com>
2017-02-26 22:55:14 +0800

    确认订单

--- src/page/confirm/Index.vue
+++ src/page/confirm/Index.vue
@@ -20,6 +20,21 @@
                     </div>
                 </div>
                 <div>
+                    <img :src="quantity>0?btn_less[1]:btn_less[2]">
+                    <span> {{quantity}} </span>
+                    <img :src="quantity<9999?btn_plus[1]:btn_plus[2]">
+                </div>
+            </div>
+            <div>
+                <div class="weui-cell phone">
+                    <div class="weui-cell__hd">
+                        <label class="weui-label">您的联系方式</label>
+                    </div>
+                    <div class="weui-cell__bd">
+                        <input class="weui-input" type="test" v-model="contact_information" placeholder="请输入联系方式" />
+                    </div>
+                </div>
+                <div class="message">
                 </div>
             </div>
         </div>
@@ -34,6 +49,12 @@
 import nan from '../product/image/nan.png'
 import nv from '../product/image/nv.png'
 import meinv from '../service-list/release/image/Artboard6.jpg'
+
+import less_1 from './image/less_1.png'
+import less_2 from './image/less_2.png'
+import plus_1 from './image/plus_1.png'
+import plus_2 from './image/plus_2.png'
+
 export default {
     components: {},
     data() {
@@ -43,6 +64,16 @@ export default {
                 2: nv
             },
             img_meinv: meinv,
+            btn_less: {
+                1: less_1,
+                2: less_2
+            },
+            btn_plus: {
+                1: plus_1,
+                2: plus_2
+            },
+            quantity: 9999,
+            contact_information: 13672888888
         }
     },
     mounted() {},
@@ -63,7 +94,7 @@ export default {
     overflow-y: auto;
     overflow-x: hidden;
     -webkit-overflow-scrolling: touch;
-    margin: 0 15px;
+    margin-left: 15px;
     .user-information,
     .service-information {
         border-bottom: 1px solid #E2E2E2;
@@ -107,28 +138,57 @@ export default {
     }
     .service-information {
         height: 70px;
-        >div>div:nth-child(1) {
-            margin-top: 15px;
-            font-size: 13px;
-            color: #333;
-        }
-        >div>div:nth-child(2) {
-            color: #A878E5;
-            margin-top: 5px;
-            span:nth-child(1) {
-                font-size: 12px;
+        position: relative;
+        >div {
+            >div:nth-child(1) {
+                margin-top: 7.5px;
+                font-size: 13px;
+                color: #333;
             }
-            span:nth-child(2) {
-                font-size: 17px;
-            }
-            span:nth-child(3) {
-                font-size: 10px;
+            >div:nth-child(2) {
+                color: #A878E5;
+                margin-top: 5px;
+                span:nth-child(1) {
+                    font-size: 12px;
+                }
+                span:nth-child(2) {
+                    font-size: 17px;
+                }
+                span:nth-child(3) {
+                    font-size: 10px;
+                }
             }
         }
-        >div:nth-child(3) {
-            font-size: 10px;
-            color: #999;
+        >div:nth-child(2) {
+            width: 100px;
+            position: absolute;
+            right: 20px;
+            top: 0;
+            bottom: 7.5px;
+            line-height: 60px;
+            text-align: right;
+            span {
+                font-size: 16px;
+                margin-bottom: 1px;
+                vertical-align: middle;
+            }
+            img {
+                vertical-align: middle;
+                width: 14px;
+                height: 14px;
+            }
         }
+    }
+    .weui-cell.phone {
+        padding-left: 0;
+        font-size: 14px;
+        .weui-input {
+            text-align: right;
+            color: #666666;
+        }
+    }
+    .weui-cell.phone:before {
+        border: 0;
     }
 }
 
