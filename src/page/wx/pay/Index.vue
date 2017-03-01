083072acf4f7a4cce31d79d8b8f42423f6deca94
LG <171736699@qq.com>
2017-03-01 21:30:08 +0800

    test

--- src/page/wx/pay/Index.vue
+++ src/page/wx/pay/Index.vue
@@ -13,37 +13,33 @@ export default {
         }
     },
     mounted() {
-       this.wxPay();
+       function onBridgeReady(){
+               WeixinJSBridge.invoke(
+                   'getBrandWCPayRequest', {
+                       "appId" : "wx2421b1c4370ec43b",     //公众号名称，由商户传入     
+                       "timeStamp":" 1395712654",         //时间戳，自1970年以来的秒数     
+                       "nonceStr" : "e61463f8efa94090b1f366cccfbbb444", //随机串     
+                       "package" : "prepay_id=u802345jgfjsdfgsdg888&total_fee=0.01",     
+                       "signType" : "MD5",         //微信签名方式:     
+                       "paySign" : "70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名 
+                   },
+                   function(res){     
+                       if(res.err_msg == "get_brand_wcpay_request：ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+                   }
+               );
+            }
+            if (typeof WeixinJSBridge == "undefined"){
+               if( document.addEventListener ){
+                   document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+               }else if (document.attachEvent){
+                   document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
+                   document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+               }
+            }else{
+               onBridgeReady();
+            }
     },
     methods: {
-        wxPay(){
-            wx.config({
-                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                appId: 'wx403ead26691402fb', // 必填，公众号的唯一标识
-                timestamp: param.timestamp, // 必填，生成签名的时间戳
-                nonceStr: param.noncestr, // 必填，生成签名的随机串
-                signature: param.signJs,// 必填，调用js签名，
-                jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，这里只写支付的
-            });
-            wx.chooseWXPay({
-                timestamp: param.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                nonceStr: param.noncestr, // 支付签名随机串，不长于 32 位
-                package: "prepay_id="+param.transNo, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-                signType: "MD5", // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                paySign: param.sign, // 支付签名
-                success: function (res) {
-                    if(res.errMsg == "chooseWXPay:ok"){
-                    //alert("支付成功");
-                        window.location.href = "/hims/weixin/pages/Order_ok.html?access_token="+getUrlParam("access_token");
-                    }else{
-                        alert(res.errMsg);
-                    }
-                },
-                cancel: function(res){
-                //alert('取消支付');
-                }
-            });
-        }
     },
     destroyed() {}
 }
