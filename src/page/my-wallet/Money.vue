7e345b663c20b3fdd463c9b1c7ec5565e1a05aec
何娇凤 <January.he@gmail.com>
2017-03-02 20:58:20 +0800

    样式

--- src/page/my-wallet/Money.vue
+++ src/page/my-wallet/Money.vue
@@ -3,20 +3,47 @@
         <div class="container-body">
             <div class="weui-cells">
                 <div class="weui-cell" href="javascript:;">
-                    <label class="weui-label class_font_size_hd">金额</label>
-                    <input class="weui-input" v-model="categor" type="text" />
-                    <label>全部提现</label>
+                    <label class="class_font_size_hd">支付宝账号</label>
+                    <input class="weui-input" v-model="alipay_account" type="text" placeholder="请输入支付宝账号" />
                 </div>
                 <div class="weui-cell" href="javascript:;">
-                    <label class="weui-label class_font_size_hd">收支明细</label>
-                    <input class="weui-input disabled-input class_font_size_bd" v-model="categor" type="text" />
-                    <div><img :src="arrow" style="width:15px;height:15px;" /></div>
+                    <label class="label">金额</label>
+                    <input class="weui-input" v-model="money" type="text" placeholder="请输入提现金额" />
+                    <a>全部提现</a>
+                </div>
+                <div class="weui-cell" href="javascript:;">
+                    <label class="weui-label">可提现金额(元):</label>
+                    <input class="weui-input" v-model="categor" type="text" />
                 </div>
             </div>
+            <button class="weui-btn weui-btn_primary">下一步</button>
         </div>
-        </div>
+    </div>
 </template>
 <style lang="less" scoped>
+.container-body {
+    .weui-cell {
+        height: 40px;
+        font-size: 13px;
+        .class_font_size_hd{
+            width:25%;
+        }
+        .label {
+            width: 12%;
+        }
+        input {
+            width: 68%;
+            height: 13px;
+            display: block;
+        }
+    }
+    .weui-btn {
+        margin-top: 30px;
+        height: 45px;
+        width: 345px;
+        background-color: #A877E6;
+    }
+}
 </style>
 <script>
 </script>
