538433cb7d141913610ff906f0d9fce095a18c74
何娇凤 <January.he@gmail.com>
2017-03-02 11:14:53 +0800

    我的锅

--- src/page/my-wallet/Money.vue
+++ src/page/my-wallet/Money.vue
@@ -1,17 +1,18 @@
 <template>
-<div><div class="container-body">
-    <div class="weui-cells">
-   
-        <div class="weui-cell" href="javascript:;">
-            <label class="weui-label class_font_size_hd">金额</label>
-            <input class="weui-input" v-model="categor" type="text" />
-            <label>全部提现</label>
-        </div>
-        <div class="weui-cell" href="javascript:;">
-            <label class="weui-label class_font_size_hd">收支明细</label>
-            <input class="weui-input disabled-input class_font_size_bd" v-model="categor" type="text" />
-            <div><img :src="arrow" style="width:15px;height:15px;" /></div>
-        </div>
+    <div>
+        <div class="container-body">
+            <div class="weui-cells">
+                <div class="weui-cell" href="javascript:;">
+                    <label class="weui-label class_font_size_hd">金额</label>
+                    <input class="weui-input" v-model="categor" type="text" />
+                    <label>全部提现</label>
+                </div>
+                <div class="weui-cell" href="javascript:;">
+                    <label class="weui-label class_font_size_hd">收支明细</label>
+                    <input class="weui-input disabled-input class_font_size_bd" v-model="categor" type="text" />
+                    <div><img :src="arrow" style="width:15px;height:15px;" /></div>
+                </div>
+            </div>
         </div>
         </div>
 </template>
