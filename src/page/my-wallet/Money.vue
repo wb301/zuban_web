2ec8539e7c5fe53722ef84226ce2d54f76e8b7bf
何俊宏 <704492613@qq.com>
2017-03-02 21:34:24 +0800

    申请提现

--- src/page/my-wallet/Money.vue
+++ src/page/my-wallet/Money.vue
@@ -1,22 +1,22 @@
 <template>
     <div>
         <div class="container-body">
-            <div class="weui-cells">
+            <div class="weui-cells" style="margin-top: 0px">
                 <div class="weui-cell" href="javascript:;">
                     <label class="class_font_size_hd">支付宝账号</label>
                     <input class="weui-input" v-model="alipay_account" type="text" placeholder="请输入支付宝账号" />
                 </div>
                 <div class="weui-cell" href="javascript:;">
                     <label class="label">金额</label>
-                    <input class="weui-input" v-model="money" type="text" placeholder="请输入提现金额" />
-                    <a>全部提现</a>
+                    <input class="weui-input" v-model="money_model" type="text" placeholder="请输入提现金额" />
+                    <label class="label" style="width: 70px" @click="allWithdraw">全部提现</label>
                 </div>
                 <div class="weui-cell" href="javascript:;">
-                    <label class="weui-label">可提现金额(元):</label>
+                    <label class="weui-label" style="width: 100%">可提现金额(元):{{userInfo.money.available}}</label>
                     <input class="weui-input" v-model="categor" type="text" />
                 </div>
             </div>
-            <button class="weui-btn weui-btn_primary">下一步</button>
+            <button class="weui-btn weui-btn_primary" @click='toWithdraw'>下一步</button>
         </div>
     </div>
 </template>
@@ -46,4 +46,74 @@
 }
 </style>
 <script>
+export default {
+    components: {},
+    data() {
+        return {
+            userInfo: NormalHelper.userInfo(),
+            money_model: "",
+            alipay_account: ""
+        }
+    },
+    mounted() {
+        $(".disabled-input").focus(function() {
+            document.activeElement.blur();
+        });
+        this.getUserInfo();
+    },
+    methods: {
+        getUserInfo() {
+
+            var param = {
+                token: this.userInfo.token
+            };
+            var p_obj = {
+                action: 'c=Zb&m=User&a=getUserInfo',
+                param: param,
+                success: (response) => {
+                    response["token"] = this.userInfo.token;
+                    NormalHelper.setUserInfo(response);
+                    this.userInfo = response;
+                },
+                fail: (response) => {
+                    weui.alert(response.msg)
+                }
+            };
+            AjaxHelper.PostRequest(p_obj);
+        },
+        toWithdraw() {
+            if(this.money_model == "" || this.money_model <= 0){
+                weui.alert("请输入提现金额");
+                return false;
+            }
+
+            if(this.alipay_account == ""){
+                weui.alert("请输入支付宝账号");
+                return false;
+            }
+
+            var param = {
+                token: this.userInfo.token,
+                price: this.money_model,
+                account: this.alipay_account,
+                from: "ZFB"
+            };
+            var p_obj = {
+                action: 'c=Zb&m=User&a=userApplyWithdraw',
+                param: param,
+                success: (response) => {
+                    this.getUserInfo();
+                    weui.alert("提现成功");
+                },
+                fail: (response) => {
+                    weui.alert(response.msg)
+                }
+            };
+            AjaxHelper.PostRequest(p_obj);
+        },
+        allWithdraw() {
+            this.money_model = this.userInfo.money.available;
+        }
+    }
+}
 </script>
