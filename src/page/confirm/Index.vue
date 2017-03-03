ed3450a6b8854cd68149065d5e486461eabd7e62
沈冬旭 <18001637008@163.com>
2017-03-03 22:17:54 +0800

    1

--- src/page/confirm/Index.vue
+++ src/page/confirm/Index.vue
@@ -60,34 +60,6 @@ import less_2 from './image/less_2.png'
 import plus_1 from './image/plus_1.png'
 import plus_2 from './image/plus_2.png'
 
-// function onBridgeReady() {
-//     WeixinJSBridge.invoke(
-//         'getBrandWCPayRequest', {
-//             "appId": "wx2421b1c4370ec43b", //公众号名称，由商户传入     
-//             "timeStamp": " 1395712654", //时间戳，自1970年以来的秒数     
-//             "nonceStr": "e61463f8efa94090b1f366cccfbbb444", //随机串     
-//             "package": "prepay_id=u802345jgfjsdfgsdg888",
-//             "signType": "MD5", //微信签名方式:     
-//             "paySign": "70EA570631E4BB79628FBCA90534C63FF7FADD89", //微信签名
-//             "total_fee": 100
-//         },
-//         function(res) {
-//             if (res.err_msg == "get_brand_wcpay_request:ok") {} // 使用以上方式判断前端返回,微信团队郑重提示:res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
-//         }
-//     );
-// }
-
-// if (typeof WeixinJSBridge == "undefined") {
-//     if (document.addEventListener) {
-//         document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-//     } else if (document.attachEvent) {
-//         document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-//         document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-//     }
-// } else {
-//     onBridgeReady();
-// }
-
 export default {
     components: {},
     data() {
@@ -169,7 +141,7 @@ export default {
             AjaxHelper.GetRequest(p_obj);
         },
         createOrder() {
-            if(this.openid.length > 0){ //TODO:并且微信浏览器打开
+            if (this.openid.length > 0) { //TODO:并且微信浏览器打开
                 var param = {
                     memo: this.memo,
                     phone: this.contact_information,
@@ -204,7 +176,7 @@ export default {
                     }
                 };
                 AjaxHelper.PostRequest(p_obj);
-            }else{
+            } else {
                 //弹出 关注公众号二维码 并提示
             }
         },
@@ -235,7 +207,7 @@ export default {
                     WeixinJSBridge.invoke('getBrandWCPayRequest', payJson,
                         function(res) {
                             console.log(res);
-                            //myApp.alert(res.err_msg);
+                            //TODO:订单回调  自己跳去
                         }
                     );
                 }, (response) => {
