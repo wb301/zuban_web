25ddb291cbfc1ca9106d3fe7effff5efcf5b74ed
何俊宏 <704492613@qq.com>
2017-03-05 21:44:26 +0800

    注销登录

--- src/page/service-list/my/Index.vue
+++ src/page/service-list/my/Index.vue
@@ -65,6 +65,15 @@
                     </div>
                 </a>
             </div>
+            <div class="weui-cells" style="margin-top:8px;" v-if="is_weixin === false">
+                <a class="weui-cell weui-cell_access" href="javascript:;" @click='toLogin'>
+                    <div class="weui-cell__bd">
+                        <p class="font_size">注销登录</p>
+                    </div>
+                    <div class="weui-cell__ft">
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </template>
@@ -79,11 +88,12 @@ export default {
         return {
             image: xiala,
             shouji: shouji,
+            is_weixin: NormalHelper.isWeixin(),
             userInfo: NormalHelper.userInfo()
         }
     },
     mounted() {
-        console.log();
+
     },
     methods: {
         myInfo() {
@@ -119,6 +129,12 @@ export default {
         myWallet() {
             this.$router.push({
                 path: '/my-wallet'
+            });
+        },
+        toLogin() {
+            NormalHelper.setUserInfo({});
+            this.$router.push({
+                path: '/login'
             });
         }
     },
