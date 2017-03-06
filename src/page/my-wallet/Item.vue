db3ab3f84e94e4f94c3d0c42c71fd9135050eec1
何娇凤 <January.he@gmail.com>
2017-03-06 20:52:50 +0800

    收支明细页

--- src/page/my-wallet/Item.vue
+++ src/page/my-wallet/Item.vue
@@ -1,44 +1,30 @@
 <template>
     <div>
-        <div class="item-entry" @click="toProductInfo(item)">
+        <div class="item-entry">
             <div class="wapper">
                 <div class="info-wapper">
-                    <div>
-                        <div class="info-top">
-                            <div>
-                                <span class="title">{{item.remark}}</span>
-                                <span class="status">{{item.create_time}}</span>
-                            </div>
-                            <div>
-                                <span class="region">{{item.price}}</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    <div><span class="title">{{item.remark}}</sapn></div>
+                    <span class="time">{{item.create_time}}</span>
+                <span v-if="item.price_type == 1 || item.price_type == 3 || item.price_type == 4" class="price">{{item.price}}元</span>
+                <span v-if="item.price_type == 2 || item.price_type == 5 || item.price_type == 6" class="price2">{{item.price}}元</span>
             </div>
         </div>
+    </div>
     </div>
 </template>
 <script>
 export default {
     props: {
-        item: {}
+        item: {},
     },
     data() {
         return {}
     },
-    // created: function() {
-    //     var map = {
-    //         "3": "收款",
-    //         "5": "提现"
-    //     };
-    //     this.item.statusName = map[this.item.price_type];
-    // },
     methods: {
-        // toProductInfo(item){
-        //     this.$router.push({
-        //         path: '/product/'+item.product_sys_code
-        //     });
+        // if(item.type !=2){
+        //     this
+        // }else{
+
         // }
     },
     // destroyed() {}
@@ -46,72 +32,43 @@ export default {
 </script>
 <style lang="less" scoped>
 .item-entry {
-    height: 117.5px;
+    height: 60px;
     position: relative;
     .wapper {
         border-bottom: 1px solid #e2e2e2;
         position: absolute;
         right: 0;
-        left: 15px;
+        left: 0px;
         top: 0;
         bottom: 0;
-        padding-top: 12.5px;
-        .img-wapper {
-            width: 90px;
-            height: 90px;
-            img {
-                width: 90px;
-                height: 90px;
-            }
-        }
+        padding-top: 0px;
         .info-wapper {
             position: absolute;
-            left: 100px;
-            right: 0;
+            left: 15px;
+            right: 15px;
             top: 12.5px;
             bottom: 15px;
-            >div {
-                height: 45px;
+            line-height: 15px;
+            .title {
+                font-size: 14px;
+                color: #333;
             }
-            .info-top {
-                >div {
-                    line-height: 15px;
-                }
-                .title {
-                    font-size: 13px;
-                    color: #333;
-                }
-                .region {
-                    font-size: 10px;
-                    color: #999;
-                }
-                .status {
-                    font-size: 10px;
-                    color: #999;
-                    position: absolute;
-                    right: 10px;
-                }
+            .time {
+                font-size: 10px;
+                color: #999;
+                margin-top: 5px;
             }
-            .info-bottom {
-                >div {
-                    line-height: 15px;
-                }
-                .price {
-                    color: #A878E5;
-                    span:nth-child(1) {
-                        font-size: 12px;
-                    }
-                    span:nth-child(2) {
-                        font-size: 17px;
-                    }
-                    span:nth-child(3) {
-                        font-size: 10px;
-                    }
-                }
-                .content {
-                    font-size: 10px;
-                    color: #999;
-                }
+            .price {
+                font-size: 14px;
+                color: #E35257;
+                position: absolute;
+                right: 10px;
+            }
+            .price2{
+                font-size: 14px;
+                color: #1CAC1B;
+                position: absolute;
+                right: 10px;
             }
         }
     }
