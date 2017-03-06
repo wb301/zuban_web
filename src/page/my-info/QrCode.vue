ff9ebb965be92225876eb1debccf1a0837357ace
何娇凤 <January.he@gmail.com>
2017-03-06 21:18:15 +0800

    二维码样式

--- src/page/my-info/QrCode.vue
+++ src/page/my-info/QrCode.vue
@@ -1,15 +1,13 @@
 <template>
     <div>
-        <div style="background: #F5F5F5;">
-            
-            <img :src="xiala" style="height: 300px;width: 300px;position: center;">
-
+        <div class="container-body">
+            <div><span>扫描下方二维码，关注租伴网公众号</span></div>
+            <img :src="xiala" />
         </div>
     </div>
 </template>
 <script>
 import xiala from '../service-list/list/image/xiala.png'
-
 export default {
     components: {
 
@@ -25,4 +23,20 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.container-body {
+    width: 100%;
+    text-align: center;
+    position: fixed;
+    top: 200px;
+    bottom: 300px;
+    span {
+        font-size: 14px;
+        color: #333;
+    }
+    img {
+        margin-top: 20px;
+        width: 150px;
+        height: 150px;
+    }
+}
 </style>
