ad142eeb73542a3628e89f63d90e1d5f038225d8
何俊宏 <704492613@qq.com>
2017-03-06 19:40:18 +0800

    中英文符号错误

--- src/page/my-info/Index.vue
+++ src/page/my-info/Index.vue
@@ -253,7 +253,7 @@ export default {
                 action: 'c=Zb&m=User&a=updUserInfo',
                 param: param,
                 success: (response) => {
-                    weui.alert("修改成功")；
+                    weui.alert("修改成功");
                     response["token"] = this.userInfo.token;
                     JSON.stringify(response);
                     NormalHelper.setUserInfo(response);
