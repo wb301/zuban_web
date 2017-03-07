efad54cb91637f500af62be12c63b40341e77f0d
LG <171736699@qq.com>
2017-03-07 22:01:16 +0800

    支付对接

--- src/page/pay/Index.vue
+++ src/page/pay/Index.vue
@@ -17,13 +17,71 @@ export default {
     },
     data() {
         return {
-            qrcode: qrcode
+            qrcode: qrcode,
+            order_no:'',
+            order_price:0,
+            orderType:1,
+            openid: NormalHelper.userInfo().wx_openid,
         }
     },
     mounted() {
+        var payInfo = NormalHelper.Get("pay");
+        this.orderType = payInfo.pay_type;
+        this.order_no = payInfo.order_no;
+        this.order_price = payInfo.all_price;
 
+        this.prePay();
     },
-    methods: {},
+    methods: {
+        prePay() {
+            var that = this;
+            if (NormalHelper.isWeixin()) {
+                var p_obj = {
+                    action: 'c=Zb&m=Order&a=prePay',
+                    param: {
+                        out_trade_no: this.order_no,
+                        total_fee: parseFloat(this.order_price) * 100,
+                        openid: this.openid
+                    }
+                };
+                var serverUrl = p_obj.serverUrl || GlobalModel.SERVER_URL;
+                Vue.http.post(serverUrl + p_obj.action, p_obj.param, {
+                    emulateJSON: true
+                }).then((response) => {
+                    console.log(response);
+                var payJson = {
+                    appId: response.body.appid,
+                    timeStamp: response.body.timeStamp + "",
+                    nonceStr: response.body.nonceStr,
+                    package: response.body.package,
+                    signType: "MD5",
+                    paySign: response.body.sign
+                };
+                WeixinJSBridge.invoke('getBrandWCPayRequest', payJson,
+                        function(res) {
+                            if(res == "get_brand_wcpay_request:ok"){
+                                weui.alert("支付成功");
+                            }else{
+                                weui.alert("支付失败");
+                            }
+                            if(this.pay_type==1){
+                                that.$router.push({
+                                    path: '/buy_orderlist'
+                                });
+                            }else {
+                                this.$router.push({
+                                    path: '/vip'
+                                });
+                            }
+                        }
+                );
+            }, (response) => {
+                    //请求异常
+                    weui.alert("支付异常!")
+                })
+            }
+        }
+    },
     destroyed() {}
 }
 </script>
